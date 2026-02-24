72b2b6d392fc80296b4b9684e989a6e79618bae4
Kakiow <xcexc111@gmail.com>
2026-02-24 19:12:20 +0700

    Percobaan2

---
+++ Pertemuan2/MahasiswaMain24.java
@@ -0,0 +1,17 @@
+package Pertemuan2;
+
+public class MahasiswaMain24 {
+    
+    public static void main(String[] args) {
+        Mahasiswa24 mhs1 = new Mahasiswa24();
+        mhs1.nama = "okta";
+        mhs1.nim = "254107020239";
+        mhs1.kelas = "D4 1F";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkaninformasi();
+        mhs1.ubahkelas("D4 1A");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkaninformasi();
+    }
+}
