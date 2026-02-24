7c80b6ae2b3a891619ffa4d1c967084045e26742
Kakiow <xcexc111@gmail.com>
2026-02-24 17:42:25 +0700

    Percobaan1

---
+++ Pertemuan2/Mahasiswa24.java
@@ -0,0 +1,35 @@
+package Pertemuan2;
+
+public class Mahasiswa24 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkaninformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("Nim: " + nim);
+        System.out.println("Ipk: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahkelas(String KelasBaru)    {
+        kelas = KelasBaru;
+    }
+
+    void updateIpk(double IpkBaru)  {
+        ipk = IpkBaru;
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else{
+            return "Kinerja kurang";
+        }
+    }
+}
