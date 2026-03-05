e071ae154796d69d16f10e3f4c58b9a8eb092be5
Kakiow <xcexc111@gmail.com>
2026-03-05 07:02:56 +0700

    Percobaan 3.4

---
+++ Pertemuan3/MataKuliah24.java
@@ -0,0 +1,15 @@
+package Pertemuan3;
+
+public class MataKuliah24 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahjam;
+
+    public MataKuliah24(String kode,String nama,int sks,int jumlahjam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahjam = jumlahjam;
+    }
+}
---
+++ Pertemuan3/MataKuliahDemo24.java
@@ -0,0 +1,36 @@
+package Pertemuan3;
+
+import java.util.Scanner;
+
+public class MataKuliahDemo24 {
+    
+    public static void main(String[] args) {
+        Scanner okta = new Scanner(System.in);
+        MataKuliah24[] arrayOfMatakuliah = new MataKuliah24[3];
+        String kode, nama, dummy;
+        int sks, jumlahjam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode            :");
+            kode = okta.nextLine();
+            System.out.print("Nama            :");  
+            nama = okta.nextLine();
+            System.out.print("Sks             :");
+            dummy = okta.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah jam      :");
+            dummy = okta.nextLine();
+            jumlahjam = Integer.parseInt(dummy);
+            System.out.println("-------------------------------------");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode              :" + arrayOfMatakuliah[i].kode);
+            System.out.println("Nama              :" + arrayOfMatakuliah[i].nama);
+            System.out.println("Sks               :" + arrayOfMatakuliah[i].sks);
+            System.out.println("Jumlah jam        :" + arrayOfMatakuliah[i].jumlahjam);
+        }
+    }
+}
