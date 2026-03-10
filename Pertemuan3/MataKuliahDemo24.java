e84b425f38cef038d84ed9a763993c4780a3f48f
Kakiow <xcexc111@gmail.com>
2026-03-10 17:18:51 +0700

    Pertanyaan 4 percobaan 3.4

--- Pertemuan3/MataKuliahDemo24.java
+++ Pertemuan3/MataKuliahDemo24.java
@@ -6,17 +6,22 @@ public class MataKuliahDemo24 {
     
     public static void main(String[] args) {
         Scanner okta = new Scanner(System.in);
-        MataKuliah24[] arrayOfMatakuliah = new MataKuliah24[3];
+        int matkul24;
+
+        System.out.println("Jumlah elemen: ");
+        matkul24 = okta.nextInt();
+
+        MataKuliah24[] arrayOfMatakuliah = new MataKuliah24[matkul24];
         
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < matkul24; i++) {
             System.out.println("Masukkan data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah[i] = new MataKuliah24();
 
             arrayOfMatakuliah[i].tambahData();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < matkul24; i++) {
             System.out.println("Data Matakuliah ke- " + (i + 1));
             arrayOfMatakuliah[i].cetakInfo();
         }
