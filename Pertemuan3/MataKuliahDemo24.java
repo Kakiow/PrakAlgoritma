b0419fa628193247c6c5c6321efab1dde22b14fa
Kakiow <xcexc111@gmail.com>
2026-03-10 16:39:52 +0700

    Pertanyaan 2 percobaan 3.4

--- Pertemuan3/MataKuliahDemo24.java
+++ Pertemuan3/MataKuliahDemo24.java
@@ -7,24 +7,13 @@ public class MataKuliahDemo24 {
     public static void main(String[] args) {
         Scanner okta = new Scanner(System.in);
         MataKuliah24[] arrayOfMatakuliah = new MataKuliah24[3];
-        String kode, nama, dummy;
-        int sks, jumlahjam;
+        
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Masukkan data Matakuliah ke-" + (i + 1));
-            System.out.print("Kode            :");
-            kode = okta.nextLine();
-            System.out.print("Nama            :");  
-            nama = okta.nextLine();
-            System.out.print("Sks             :");
-            dummy = okta.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah jam      :");
-            dummy = okta.nextLine();
-            jumlahjam = Integer.parseInt(dummy);
-            System.out.println("-------------------------------------");
+            arrayOfMatakuliah[i] = new MataKuliah24();
 
-            arrayOfMatakuliah[i] = new MataKuliah24(kode, nama, sks, jumlahjam);
+            arrayOfMatakuliah[i].tambahData();
         }
 
         for (int i = 0; i < 3; i++) {
