fb08451eda778c427bb9d6577571cc37dad3c971
Kakiow <xcexc111@gmail.com>
2026-03-15 12:26:17 +0700

    Tugas 1

---
+++ Pertemuan3/Dosen24.java
@@ -0,0 +1,15 @@
+package Pertemuan3;
+
+public class Dosen24 {
+    String kode;
+    String nama;
+    Boolean JenisKelamin;
+    int usia;
+
+    public Dosen24(String kode, String nama, Boolean JenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.JenisKelamin = JenisKelamin;
+        this.usia = usia;
+    }
+}
