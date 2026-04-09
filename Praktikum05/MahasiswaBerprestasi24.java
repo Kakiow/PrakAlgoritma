a3b442198b32e546101aa1ab97f120622dc9839b
Kakiow <xcexc111@gmail.com>
2026-04-09 20:29:42 +0700

    Praktikum 2 bubble sort

--- Praktikum05/MahasiswaBerprestasi24.java
+++ Praktikum05/MahasiswaBerprestasi24.java
@@ -21,8 +21,8 @@ public class MahasiswaBerprestasi24 {
     }
 
     void bubbleSort(){
-        for (int i = 0; i < listMhs.length; i++) {
-            for (int j = 1; j < listMhs.length; j++) {
+        for (int i = 0; i < listMhs.length-1; i++) {
+            for (int j = 1; j < listMhs.length-i; j++) {
                 if (listMhs[j].ipk>listMhs[j-1].ipk) {
                     Mahasiswa24 tmp = listMhs[j];
                     listMhs[j]=listMhs[j-1];
