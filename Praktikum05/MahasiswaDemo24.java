69174d8f87b249124d514dc08bfe54f9f1cc57e7
Kakiow <xcexc111@gmail.com>
2026-04-09 21:40:16 +0700

    Praktikum 2 selection sort

--- Praktikum05/MahasiswaDemo24.java
+++ Praktikum05/MahasiswaDemo24.java
@@ -33,5 +33,9 @@ public class MahasiswaDemo24 {
         System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) :");
         list.bubbleSort();
         list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        list.selectionSort();
+        list.tampil();
     }
 }
