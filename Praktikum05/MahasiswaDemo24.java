27f775a838ac76f543ef7aef5c1596c1a73919ba
Kakiow <xcexc111@gmail.com>
2026-04-09 21:08:49 +0700

    Pertanyaan 2 praktikum 2 bubble sort

--- Praktikum05/MahasiswaDemo24.java
+++ Praktikum05/MahasiswaDemo24.java
@@ -1,19 +1,31 @@
 package Praktikum05;
 
+import java.util.Scanner;
+
 public class MahasiswaDemo24 {
     public static void main(String[] args) {
+        Scanner okta = new Scanner(System.in);
         MahasiswaBerprestasi24 list = new MahasiswaBerprestasi24();
-        Mahasiswa24 m1 = new Mahasiswa24("123", "Zidan", "2A", 3.2);
-        Mahasiswa24 m2 = new Mahasiswa24("124", "Ayu", "2A", 3.5);
-        Mahasiswa24 m3 = new Mahasiswa24("125", "Sofi", "2A", 3.1);
-        Mahasiswa24 m4 = new Mahasiswa24("126", "Sita", "2A", 3.9);
-        Mahasiswa24 m5 = new Mahasiswa24("127", "Miki", "2A", 3.7);
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        int Mhs = 5;
+        
+        for (int i = 0; i < Mhs; i++) {
+            System.out.println("Data mahasiswa ke-" + (i + 1));
+            System.out.print("Nama:");
+            String nama = okta.nextLine();
+            System.out.print("NIM:");
+            String nim = okta.nextLine();
+            System.out.print("IPK:");
+            double ipk = okta.nextDouble();
+            okta.nextLine();
+            System.out.print("Kelas:");
+            String kelas = okta.nextLine();
+
+            Mahasiswa24 m = new Mahasiswa24(nim, nama, kelas, ipk);
+
+            list.tambah(m);
+        }
+
 
         System.out.println("Data mahasiswa sebelum sorting: ");
         list.tampil();
