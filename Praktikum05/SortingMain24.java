2759097a4278fc8301c234b11633a427ac27c98f
Kakiow <xcexc111@gmail.com>
2026-04-09 19:25:52 +0700

    Praktikum 1 c

--- Praktikum05/SortingMain24.java
+++ Praktikum05/SortingMain24.java
@@ -26,7 +26,7 @@ public class SortingMain24 {
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
 
-        System.out.println("Data awal 2");
+        System.out.println("Data awal 3");
         dataurut3.tampil();
         dataurut3.insertionSort();
         System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
