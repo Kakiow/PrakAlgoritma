07f2ebfa8914ec958cef75bc07077ae646bff839
Kakiow <xcexc111@gmail.com>
2026-04-09 19:25:11 +0700

    Praktikum 1 c

--- Praktikum05/SortingMain24.java
+++ Praktikum05/SortingMain24.java
@@ -6,9 +6,13 @@ public class SortingMain24 {
 
         int b[]= {30, 20, 2, 8, 14};
 
+        int c[]= {40, 10, 4, 9, 3};
+
         Sorting24 dataurut1 = new Sorting24(a, a.length);
 
         Sorting24 dataurut2 = new Sorting24(b, b.length);
+
+        Sorting24 dataurut3 = new Sorting24(c, c.length);
 
         System.out.println("Data awal 1");
         dataurut1.tampil();
@@ -18,8 +22,14 @@ public class SortingMain24 {
 
         System.out.println("Data awal 2");
         dataurut2.tampil();
-        dataurut2.bubbleSort();
+        dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        System.out.println("Data awal 2");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
