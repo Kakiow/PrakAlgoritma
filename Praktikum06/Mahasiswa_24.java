1b0888014b363f33471b2ea8d0f8ed99407a7c44
Kakiow <xcexc111@gmail.com>
2026-04-10 21:19:03 +0700

    Praktikum 1 sequential searching

---
+++ Praktikum06/Mahasiswa_24.java
@@ -0,0 +1,26 @@
+package Praktikum06;
+
+public class Mahasiswa_24 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa_24(){
+
+    }
+
+    Mahasiswa_24(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
