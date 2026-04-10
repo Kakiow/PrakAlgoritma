f08122542921484d6fe0ea4368cec8ac3bc90dc3
Kakiow <xcexc111@gmail.com>
2026-04-10 21:19:26 +0700

    Praktikum 1 sequential searching

---
+++ Praktikum06/MahasiswaDemo_24.java
@@ -0,0 +1,40 @@
+package Praktikum06;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo_24 {
+    public static void main(String[] args) {
+        MahasiswaBerprestasi_24 list = new MahasiswaBerprestasi_24();
+        Scanner okta = new Scanner(System.in);
+        int jumMhs=5;
+
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" +(i + 1));
+            System.out.print("NIM     :");
+            String nim = okta.nextLine();
+            System.out.print("Nama    :");
+            String nama = okta.nextLine();
+            System.out.print("Kelas   :");
+            String kelas = okta.nextLine();
+            System.out.print("IPK     :");
+            String ip = okta.nextLine();
+            Double ipk = Double.parseDouble(ip);
+            System.out.println("------------------------------");
+            list.tambah (new Mahasiswa_24(nim, nama, kelas, ipk));
+        }
+
+        list.tampil();
+        System.out.println("-------------------------------------");
+        System.out.println("Pencarian Data");
+        System.out.println("-------------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari: ");
+        System.out.println("IPK: ");
+        double cari = okta.nextDouble();
+
+        System.out.println("menggunakan sequential searching");
+        double posisi = list.sequentialSearching(cari);
+        int pss= (int)posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
+    }
+}
