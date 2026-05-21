e69cd56c2f05074a1cca464aaeca90b2f99a9b4e
Kakiow <xcexc111@gmail.com>
2026-05-21 20:41:43 +0700

    Percobaan 2

--- Jobsheet12/DoubleLinkedList24.java
+++ Jobsheet12/DoubleLinkedList24.java
@@ -85,4 +85,32 @@ public class DoubleLinkedList24 {
             current = current.prev;
         }
     }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked List kosong.");
+            return;
+        }
+
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked List kosong.");
+            return;
+        }
+
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+    }
 }
