af3e121f243d7329dfb34885c60d40109cf5f5fb
Kakiow <xcexc111@gmail.com>
2026-05-21 19:49:35 +0700

    Percobaan 1

---
+++ Jobsheet12/DoubleLinkedList24.java
@@ -0,0 +1,75 @@
+package Jobsheet12;
+
+public class DoubleLinkedList24 {
+    Node24 head;
+    Node24 tail;
+
+    public DoubleLinkedList24() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa24 data) {
+        Node24 newNode = new Node24(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa24 data) {
+        Node24 newNode = new Node24(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void insertAfter(String keyNim, Mahasiswa24 data) {
+        Node24 current = head;
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+        if (current == null) {
+            System.out.println("Data dengan NIM " + keyNim + " tidak ditemukan.");
+            return;
+        }
+
+        Node24 newNode = new Node24(data);
+
+        if (current == tail) {
+            newNode.prev = current;
+            current.next = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = current;
+            newNode.next = current.next;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+        System.out.println("Data berhasil disisipkan setelah NIM " + keyNim);
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih 4kosong.");
+            return;
+        }
+
+        Node24 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+}
---
+++ Jobsheet12/DoubleLinkedListMain24.java
@@ -0,0 +1,69 @@
+package Jobsheet12;
+
+import java.util.Scanner;
+
+public class DoubleLinkedListMain24 {
+
+    public static Mahasiswa24 inputMahasiswa(Scanner okta) {
+        System.out.println("Masukkan NIM    : ");
+        String nim = okta.nextLine();
+        System.out.println("Masukkan Nama   : ");
+        String nama = okta.nextLine();
+        System.out.println("Masukkan Kelas  : ");
+        String kelas = okta.nextLine();
+        System.out.println("Masukkan IPK    : ");
+        double ipk = okta.nextDouble();
+        okta.nextLine();
+        return new Mahasiswa24(nim, nama, kelas, ipk);
+    }
+    public static void main(String[] args) {
+        Scanner okta = new Scanner(System.in);
+        DoubleLinkedList24 list = new DoubleLinkedList24();
+        int pilihan;
+
+        do {
+            System.out.println("\n===== MENU DOUBLE LINKED LIST =====");
+            System.out.println("1. Tambah data di awal");
+            System.out.println("2. Tambah data di akhir");
+            System.out.println("3. Sisipkan data di tengah (setelah NIM");
+            System.out.println("4. Hapus data di awal");
+            System.out.println("5. Hapus data di akhir");
+            System.out.println("6. Tampilkan data");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu : ");
+            pilihan = okta.nextInt();
+            okta.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    Mahasiswa24 mhsAwal = inputMahasiswa(okta);
+                    list.addFirst(mhsAwal);
+                    break;
+                case 2:
+                    Mahasiswa24 mhsAkhir = inputMahasiswa(okta);
+                    list.addLast(mhsAkhir);
+                    break;
+                case 3:
+                    System.out.print("Masukkan NIM yang dicari : ");
+                    String keyNim = okta.nextLine();
+                    System.out.println("Masukkan data baru: ");
+                    Mahasiswa24 dataBaru = inputMahasiswa(okta);
+                    list.insertAfter(keyNim, dataBaru);
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    list.print();
+                    break;
+                case 0:
+                    System.out.println("Program selesai.");
+                    break;
+                default:
+                    System.out.println("Menu tidak valid.");
+                    break;
+            }
+        } while (pilihan != 0);
+    }
+}
