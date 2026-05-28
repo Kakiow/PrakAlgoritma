e064be2a1230a512284dac3ea7a331c7110bb43a
Kakiow <xcexc111@gmail.com>
2026-05-29 00:12:00 +0700

    CM2

---
+++ CM2/Pembeli24.java
@@ -0,0 +1,13 @@
+package CM2;
+
+public class Pembeli24 {
+    String namaPembeli;
+    String NoHp;
+    int antrian;
+    
+    public Pembeli24(String b, String c, int antrian) {
+        namaPembeli = b;
+        NoHp = c;
+        this.antrian = antrian;
+    }
+}
---
+++ CM2/Pesanan24.java
@@ -0,0 +1,15 @@
+package CM2;
+
+public class Pesanan24 {
+    int kodePesanan;
+    String namaPesanan;
+    int harga;
+    int antrian;
+
+    public Pesanan24(int a, String b, int d, int antrian) {
+        kodePesanan = a;
+        namaPesanan = b;
+        harga = d;
+        this.antrian = antrian;
+    }
+}
